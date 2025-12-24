2e39641ba537895d6746063caa78f1d5a2687daf
pedroblandim <pedro.blandim@gmail.com>
2025-12-24 17:57:59 -0300

    feat: print Info Hash

--- src/bencode/bencode_types.rs
+++ src/bencode/bencode_types.rs
@@ -2,6 +2,7 @@ use std::collections::HashMap;
 
 use serde_json::{Map, Number, Value};
 
+#[derive(Debug)]
 pub enum BencodeTypes {
     Integer(i64),
     ByteString(Vec<u8>),
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use std::fs;
 use std::{env, str::FromStr};
 
+use sha1::{Digest, Sha1};
+
 use crate::{bencode::bencode_types::BencodeTypes, commands::Commands};
 
 mod bencode;
@@ -9,7 +11,10 @@ mod commands;
 // Usage: your_program.sh decode "<encoded_value>"
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let command_string = &args[1];
+    let command_string = match args.get(1) {
+        Some(s) => s,
+        None => "info",
+    };
 
     let Ok(command) = Commands::from_str(command_string) else {
         println!("unknown command: {}", args[1]);
@@ -27,7 +32,11 @@ fn main() {
             println!("{}", json_value.to_string());
         }
         Commands::Info => {
-            let file_path = &args[2];
+            let file_path = match args.get(2) {
+                Some(p) => p,
+                None => "sample.torrent",
+            };
+
             let contents = fs::read(file_path).expect(&format!("Could not read file {file_path}"));
 
             let decoded_value = BencodeTypes::decode(contents.clone());
@@ -38,7 +47,13 @@ fn main() {
 
             let deserialized_info = BencodeTypes::deserialize_from_json(info).unwrap();
 
+            // let mut encoded_info = b"".to_vec();
+            // encoded_info.append(&mut BencodeTypes::encode(&deserialized_info));
+            // encoded_info.append(&mut b"".to_vec());
+
             let encoded_info = BencodeTypes::encode(&deserialized_info);
+
+            let info_hash = Sha1::digest(encoded_info.clone());
 
             println!(
                 "Tracker URL: {}",
@@ -48,19 +63,8 @@ fn main() {
                     .to_string()
                     .trim_matches('\"')
             );
-            println!(
-                "Length: {}",
-                decoded_json.get("info").unwrap().get("length").unwrap()
-            );
-
-            println!("Info: {:?}", decoded_json.get("info").unwrap());
-            println!("Encoded Info: {:?}", str::from_utf8(&encoded_info).unwrap());
-
-            let d = BencodeTypes::decode(contents.clone());
-            let e = BencodeTypes::encode(&d);
-            println!("\n\n");
-            println!("{:?}\n", String::from_utf8_lossy(&e));
-            println!("{:?}", String::from_utf8_lossy(&contents));
+            println!("Length: {}", info.get("length").unwrap());
+            println!("Info Hash: {info_hash:x}");
         }
     }
 }
