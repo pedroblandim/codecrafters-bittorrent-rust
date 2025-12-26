8b75eb29476005532c1625b9488f81ed7a6af84e
pedroblandim <pedro.blandim@gmail.com>
2025-12-25 21:28:44 -0300

    feat: implement get_byte_string, get_integer, get_dict and get_list

--- src/bencode/bencode_types.rs
+++ src/bencode/bencode_types.rs
@@ -1,4 +1,4 @@
-use std::collections::HashMap;
+use std::{collections::HashMap, panic};
 
 use serde_json::{Map, Number, Value};
 
@@ -11,6 +11,66 @@ pub enum BencodeTypes {
 }
 
 impl BencodeTypes {
+    pub fn get_byte_string(&self, key: &str) -> Vec<u8> {
+        let key_bytes = key.as_bytes().to_vec();
+        let bencode_type = self.get(&key_bytes);
+
+        match bencode_type {
+            BencodeTypes::ByteString(string) => string.to_owned(),
+            BencodeTypes::Integer(_) => panic!("Key {key} maps to an Integer"),
+            BencodeTypes::List(_) => panic!("Key {key} maps to a List"),
+            BencodeTypes::Dictionary(_) => panic!("Key {key} maps to a Dictionary"),
+        }
+    }
+
+    pub fn get_integer(&self, key: &str) -> i64 {
+        let key_bytes = key.as_bytes().to_vec();
+        let bencode_type = self.get(&key_bytes);
+
+        match bencode_type {
+            BencodeTypes::ByteString(_) => panic!("Key {key} maps to an ByteString"),
+            BencodeTypes::Integer(number) => *number,
+            BencodeTypes::List(_) => panic!("Key {key} maps to a List"),
+            BencodeTypes::Dictionary(_) => panic!("Key {key} maps to a Dictionary"),
+        }
+    }
+
+    pub fn get_dict(&self, key: &str) -> &BencodeTypes {
+        let key_bytes = key.as_bytes().to_vec();
+        let bencode_type = self.get(&key_bytes);
+
+        match bencode_type {
+            BencodeTypes::ByteString(_) => panic!("Key {key} maps to an ByteString"),
+            BencodeTypes::Integer(_) => panic!("Key {key} maps to an Integer"),
+            BencodeTypes::List(_) => panic!("Key {key} maps to a List"),
+            BencodeTypes::Dictionary(_) => (),
+        }
+
+        bencode_type
+    }
+
+    #[allow(dead_code)]
+    pub fn get_list(&self, key: &str) -> &BencodeTypes {
+        let key_bytes = key.as_bytes().to_vec();
+        let bencode_type = self.get(&key_bytes);
+
+        match bencode_type {
+            BencodeTypes::ByteString(_) => panic!("Key {key} maps to an ByteString"),
+            BencodeTypes::Integer(_) => panic!("Key {key} maps to an Integer"),
+            BencodeTypes::List(_) => (),
+            BencodeTypes::Dictionary(_) => panic!("Key {key} maps to a Dictionary"),
+        }
+
+        bencode_type
+    }
+
+    fn get(&self, key: &Vec<u8>) -> &BencodeTypes {
+        match &self {
+            BencodeTypes::Dictionary(map) => map.get(key).expect("Key {key} not found"),
+            _ => panic!("Calling get on a BencodeType that is not a Dictionary"),
+        }
+    }
+
     pub fn serialize_to_json(&self) -> Value {
         match self {
             Self::Integer(number) => Value::Number(Number::from(*number)),
